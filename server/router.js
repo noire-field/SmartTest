1e5568042352006a868b697f7988406f5990be23
bismarcko <noirefield@gmail.com>
2019-05-03 10:59:30 +0700

    Working on QUestions

--- server/router.js
+++ server/router.js
@@ -71,7 +71,7 @@ module.exports.register = function(app) {
                 res.render('dashboard/index', {
                     page: 'index',
                     head_title: 'Trang quản lý - ' + config.APP_NAME,
-                    user: req.usergit
+                    user: req.user
                 })
                 break;
             case 'users':
@@ -80,12 +80,16 @@ module.exports.register = function(app) {
             case 'subjects':
                 Controller_Subjects(0, action, id, req, res, next);
                 break;
+            case 'quests':
+                Controller_Quests(0, action, id, req, res, next);
+                break;
             default:
                 res.render('error', { errorMessage: 'Trang này không tồn tại.' });
                 break;
         }
     });
 
+    // Action sections (POST)
     app.post('/dashboard/:page?/:action?/:id?', (req, res, next) => {
         if(!req.isAuthenticated()) // Check login
             return res.redirect('/login?redirect=/dashboard');
@@ -100,6 +104,9 @@ module.exports.register = function(app) {
                 break;
             case 'subjects':
                 Controller_Subjects(1, action, id, req, res, next);
+                break;
+            case 'quests':
+                Controller_Quests(1, action, id, req, res, next);
                 break;
             default:
                 return res.redirect('/');
@@ -450,6 +457,258 @@ function Controller_Subjects(type, action, id, req, res, next)
             user: req.user,
             ...extra
         });
+    }
+
+    function Render_EditPage(id, res, req, extra={}) {
+        if(!id) return res.redirect('/dashboard/subjects');
+
+        QueryNow(`SELECT s.SubjectID, s.SubjectName, u.FirstName, u.LastName FROM subjects s INNER JOIN users u ON s.OwnerID = u.UserID WHERE s.SubjectID = ?${req.user.RoleType >= 2 ? '' : ` AND s.OwnerID = '${req.user.UserID}'`}`, [id])
+        .then((rows) => {
+            if(rows.length <= 0)
+                return res.redirect('/dashboard/subjects');
+
+            res.render('dashboard/subjects/edit', {
+                page: 'subjects',
+                head_title: `Chỉnh sửa bộ đề - ${config.APP_NAME}`,
+                user: req.user,
+                editSubject: rows[0],
+                ...extra
+            });
+        })
+        .catch((error) => {
+            Log(error);
+            ErrorHandler(res, 'Oops... Something went wrong...');
+        })
+    }
+
+    function Render_DeletePage(id, res, req, extra={}) {
+        if(!id) return res.redirect('/dashboard/subjects');
+
+        QueryNow(`SELECT s.SubjectID, s.SubjectName, u.FirstName, u.LastName FROM subjects s INNER JOIN users u ON s.OwnerID = u.UserID WHERE s.SubjectID = ?${req.user.RoleType >= 2 ? '' : ` AND s.OwnerID = '${req.user.UserID}'`}`, [id])
+        .then((rows) => {
+            if(rows.length <= 0)
+                return res.redirect('/dashboard/subjects');
+
+            res.render('dashboard/subjects/delete', {
+                page: 'subjects',
+                head_title: `Xóa bộ đề - ${config.APP_NAME}`,
+                user: req.user,
+                editSubject: rows[0],
+                ...extra
+            });
+        })
+        .catch((error) => {
+            Log(error);
+            ErrorHandler(res, 'Oops... Something went wrong...');
+        })
+    }
+}
+
+
+function Controller_Quests(type, action, id, req, res, next) 
+{
+    if(req.user.RoleType < 1)
+        return res.redirect('/dashboard');
+
+    if(type == 0) { // GET
+        switch(action)
+        {
+            case null:
+            case undefined: // View list
+                var page = req.query.page ? req.query.page : 1;
+                var pagination = [page, 1];
+    
+                QueryNow(`SELECT COUNT(QuestID) AS TOTAL FROM questions${req.user.RoleType >= 2 ? `` : ` WHERE OwnerID = '${req.user.UserID}'`}`)
+                .then((rows) => {
+                    console.log(rows[0].TOTAL);
+                    var { START, LIMIT } = GetPageLimit(page, rows[0].TOTAL, config.ITEM_PER_PAGE);
+                    pagination = [page, Math.ceil(rows[0].TOTAL / config.ITEM_PER_PAGE)];
+
+                    return QueryNow(`SELECT q.QuestID, q.QuestContent, s.SubjectName, u.FirstName, u.LastName FROM questions q INNER JOIN subjects s ON q.SubjectID = s.SubjectID INNER JOIN users u ON q.OwnerID = u.UserID${req.user.RoleType >= 2 ? `` : ` WHERE s.OwnerID = '${req.user.UserID}'`} LIMIT ${START}, ${LIMIT}`);
+                })
+                .then((rows) => {
+                    res.render('dashboard/quests/index', {
+                        page: 'quests',
+                        head_title: `Quản lý câu hỏi - ${config.APP_NAME}`,
+                        user: req.user,
+                        questList: rows,
+                        pagination: { 
+                            URL: '/dashboard/quests?page=',
+                            CURRENT: pagination[0], 
+                            TOTAL: pagination[1] 
+                        }
+                    });
+                })
+                .catch((error) => {
+                    Log(error);
+                    ErrorHandler(res, 'Oops... Something went wrong...');
+                })
+    
+                break;
+            case 'add':
+                Render_AddPage(id, res, req, { status: 'none' });
+                break;
+            case 'edit':
+                Render_EditPage(id, res, req, { status: 'none' });
+                break;
+            case 'delete':
+                Render_DeletePage(id, res, req, { status: 'none' });
+                break;
+            default:
+                res.render('error', { errorMessage: 'Trang này không tồn tại.' });
+                break;
+        }
+    } else { // POST
+        switch(action)
+        {
+            case 'add':
+                var errors = [];
+
+                let quest = {
+                    SubjectID: req.body['input-subjectid'] != undefined ? req.body['input-subjectid'] : 0,
+                    Content: req.body['input-questcontent'],
+                    Tags: [],
+                    Answers: []
+                };
+
+                // Tags
+                var splitTags = req.body['input-questtags'].split(',');
+                for(tag of splitTags) {
+                    var trimTag = tag.trim();
+                    if(trimTag.length > 0)
+                        quest.Tags.push(trimTag);
+                }
+
+                // Answers
+                for(let i = 0; i < 4; i++) {
+                    if(req.body[`input-questans[${i}][content]`].length > 0) {
+                        quest.Answers.push({ 
+                            CONTENT: req.body[`input-questans[${i}][content]`],
+                            CORRECT: req.body[`input-questans[${i}][correct]`] != undefined ? true : false
+                        });
+                    }
+                }
+
+                if(quest.SubjectID <= 0)
+                    errors.push(`Bộ đề không hợp lệ, vui lòng chọn bộ đề`);
+                if(quest.Content.length <= 0 || quest.Content.length >= 128)
+                    errors.push(`Nội dung câu hỏi phải từ 1 đến 128 ký tự`);
+                if(quest.Tags.length <= 0 || quest.Tags.length > 5)
+                    errors.push(`Câu hỏi chưa có thẻ, vui lòng thêm thẻ, tối đa 5 thẻ`);
+                if(quest.Answers.length <= 0)
+                    errors.push(`Chưa có câu trả lời nào được nhập vào`);
+                else {
+                    let CorrectAnswer = false;
+                    for(let i = 0; i < quest.Answers.length; i++) {
+                        if(quest.Answers[i].CORRECT == true) {
+                            CorrectAnswer = true;
+                            break;
+                        }
+                    }
+
+                    if(!CorrectAnswer)
+                        errors.push(`Phải có ít nhất một câu hỏi đúng`);
+                }
+
+                if(errors.length <= 0) {
+                    /*
+                    QueryNow(`INSERT INTO subjects (SubjectName, OwnerID) VALUES(?, ?)`, [thisSubject.SubjectName, req.user.UserID])
+                    .then((rows) => {
+                        return res.redirect('/dashboard/subjects');
+                    }).catch((error) => {
+                        Log(error);
+                        return Render_AddPage(id, res, req, { status: 'error', errors: ['Không thể thêm do lỗi truy vấn #1'] });
+                    })*/
+                } else {
+                    return Render_AddPage(id, res, req, { status: 'error', errors: errors });
+                }
+
+                console.log(quest);
+            
+                break;
+            case 'edit':
+                if(!id) return res.redirect('/dashboard/subjects');
+
+                QueryNow(`SELECT s.SubjectID, s.SubjectName, u.FirstName, u.LastName FROM subjects s INNER JOIN users u ON s.OwnerID = u.UserID WHERE s.SubjectID = ?${req.user.RoleType >= 2 ? '' : ` AND s.OwnerID = '${req.user.UserID}'`}`, [id])
+                .then((rows) => {
+                    if(rows.length <= 0)
+                        return res.redirect('/dashboard/subjects');
+
+                    var errors = [];
+
+                    let thisSubject = {
+                        SubjectName: req.body['input-subjectname']
+                    };
+                    
+                    if(!Subject.SubjectName.IsValidName(thisSubject.SubjectName))
+                        errors.push(`Tên bộ đề phải từ ${Subject.SubjectName.MIN} đến ${Subject.SubjectName.MAX} ký tự`);
+
+                    if(errors.length <= 0) {
+                        QueryNow(`UPDATE subjects SET SubjectName = ? WHERE SubjectID = ?`, [thisSubject.SubjectName, id])
+                        .then((rows) => {
+                            return Render_EditPage(id, res, req, { status: 'success' });
+                        }).catch((error) => {
+                            Log(error);
+                            return Render_EditPage(id, res, req, { status: 'error', errors: ['Không thể cập nhật do lỗi truy vấn #1'] });
+                        })
+                    } else {
+                        return Render_EditPage(id, res, req, { status: 'error', errors: errors });
+                    }
+                })
+                .catch((error) => {
+                    Log(error);
+                    return Render_EditPage(id, res, req, { status: 'error', errors: ['Không thể cập nhật do lỗi truy vấn #2'] });
+                })
+    
+                break;
+            case 'delete':
+                if(!id) return res.redirect('/dashboard/subjects');
+
+                QueryNow(`SELECT s.SubjectID, s.SubjectName, u.FirstName, u.LastName FROM subjects s INNER JOIN users u ON s.OwnerID = u.UserID WHERE s.SubjectID = ?${req.user.RoleType >= 2 ? '' : ` AND s.OwnerID = '${req.user.UserID}'`}`, [id])
+                .then((rows) => {
+                    if(rows.length <= 0)
+                        return res.redirect('/dashboard/subjects');
+
+                    var errors = [];
+
+                    if(errors.length <= 0) {
+                        var q = QueryNow(`DELETE FROM subjects WHERE SubjectID = ?`,[id]);
+
+                        q.then((rows) => {
+                            return res.redirect('/dashboard/subjects');
+                        }).catch((error) => {
+                            Log(error);
+                            return Render_DeletePage(id, res, req, { status: 'error', errors: ['Không thể cập nhật do lỗi truy vấn #1'] });
+                        })
+                    } else {
+                        return Render_DeletePage(id, res, req, { status: 'error', errors: errors });
+                    }
+                })
+                .catch((error) => {
+                    Log(error);
+                    return Render_DeletePage(id, res, req, { status: 'error', errors: ['Không thể xóa do lỗi truy vấn #2'] });
+                })
+
+                break;
+        }
+    }
+
+    function Render_AddPage(id, res, req, extra={}) {
+        // Find subjects first
+        QueryNow(`SELECT s.SubjectID, s.SubjectName, u.FirstName, u.LastName FROM subjects s INNER JOIN users u ON s.OwnerID = u.UserID${req.user.RoleType >= 2 ? `` : ` WHERE s.OwnerID = '${req.user.UserID}'`}`)
+        .then((rows) => {
+            res.render('dashboard/quests/add', {
+                page: 'quests',
+                head_title: `Thêm câu hỏi - ${config.APP_NAME}`,
+                user: req.user,
+                subjectList: rows,
+                ...extra
+            });
+        })
+        .catch((error) => {
+            Log(error);
+            ErrorHandler(res, 'Oops... Something went wrong...');
+        })
     }
 
     function Render_EditPage(id, res, req, extra={}) {
