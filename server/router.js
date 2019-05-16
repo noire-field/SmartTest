050fd9a2f47a1329589f8e1e53590ce76ac18362
Bismarcko <noirefield@gmail.com>
2019-05-16 16:50:08 +0700

    Completed Registration

--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ const { Log } = require('./utils/logger');
 const config = require('./../config');
 const { QueryNow, GetPageLimit } = require('./database');
 
+const User = require('./utils/user');
 const { Controller_Users } = require('./controllers/users');
 const { Controller_Subjects } = require('./controllers/subjects');
 const { Controller_Quests } = require('./controllers/quests');
@@ -10,8 +11,63 @@ const { Controller_Tests } = require('./controllers/tests');
 
 module.exports.register = function(app) {
     app.get('/register', (req, res) => {
-        console.log(req.sessionID);
-        res.send("OK");
+        if(req.isAuthenticated()) 
+            return req.query.redirect ? res.redirect(req.query.redirect) : res.redirect('/');
+
+        res.render('register', {
+            head_title: 'Đăng ký - ' + config.APP_NAME,
+            redirect: req.query.redirect
+        });
+    });
+
+    app.post('/register', (req, res, next) => {
+        var errors = [];
+        
+        try {
+            var thisUser = {
+                Username: req.body.username,
+                Password: req.body.password,
+                Email: "",
+                RoleType: req.body.roleType,
+                FirstName: req.body.firstName,
+                LastName: req.body.lastName,
+                StudentID: req.body.studentId
+            };
+    
+            if(!User.IsValidUsername(thisUser.Username))
+                errors.push(`Tên tài khoản không hợp lệ (Chỉ gồm chữ, số, gạch dưới và ${config.LOGIN.USERNAME.MIN}-${config.LOGIN.USERNAME.MAX} ký tự)`);
+            if(thisUser.Password.length > 0 && !User.IsValidPassword(thisUser.Password))
+                errors.push(`Mật khẩu không hợp lệ (Chỉ ${config.LOGIN.PASSWORD.MIN}-${config.LOGIN.PASSWORD.MAX} ký tự)`);
+            //if(!User.IsValidEmail(thisUser.Email))
+            //    errors.push(`Email không hợp lệ`);
+            if(!User.IsValidRole(thisUser.RoleType) || thisUser.RoleType > 1)
+                errors.push(`Quyền hạn không hợp lệ`);
+            if(!User.IsValidName(thisUser.FirstName) || !User.IsValidName(thisUser.LastName))
+                errors.push(`Tên hoặc Họ không hợp lệ`);
+            if(!User.IsValidStudentID(thisUser.StudentID))
+                errors.push(`Mã Sinh Viên không hợp lệ`); 
+        } catch(e) {
+            return res.send({ status: false, message: "Có gì đó bị hỏng, vui lòng liên hệ quản trị viên" });
+        }
+
+        if(errors.length > 0) 
+            return res.send({ status: false, message: errors[0] });
+
+        QueryNow(`SELECT UserID FROM users WHERE Username = ?`, [thisUser.Username])
+        .then((rows) => {
+            if(rows.length > 0) 
+                return res.send({ status: false, message: "Tên tài khoản đã có người sử dụng" });
+
+            return QueryNow("INSERT INTO users (Username, Password, FirstName, LastName, RoleType, RegisteredDate, StudentID) VALUES(?,?,?,?,?,NOW(),?);",
+            [thisUser.Username, thisUser.Password, thisUser.FirstName, thisUser.LastName, thisUser.RoleType, thisUser.StudentID]);
+        })
+        .then((rows) => {
+            return res.send({ status: true });
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.send({ status: false, message: "Không thể kiểm tra trùng lặp tên" });
+        })
     });
 
     // Login page
